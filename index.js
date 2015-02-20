9419d45a3801c67e3da0672416aab69ee8ba6182
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-02-20 12:40:25 +0100

    Creating an instance of DiffSyncHandler.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var WebSocketServer = require('ws').Server,
     express = require('express'),
     app = express();
+var DiffSyncHandler = require('./lib/diff-sync-handler');
 
 app.use(express.static('public'));
 
 
-var server = app.listen(1138, function () {
+var server = app.listen(7777, '0.0.0.0', function () {
     console.log('Server UP at ' + server.address().port);
 });
 
-var wss = new WebSocketServer({server: server});
-var diffSyncHandler = require('./lib/diff-sync-handler');
-
-
+var wss = new WebSocketServer({server: server, path: "/sync"});
+var diffSyncHandler = new DiffSyncHandler();
 
 wss.on('connection', function (socket) {
     console.log('Client Connected');
