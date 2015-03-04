5f5133b27023cf117ee8dff404c9a1107952e249
Lucas Holmquist <lholmqui@redhat.com>
2015-03-04 10:52:49 -0500

    cleanup synchronizers

--- tests/synchronizers/diff-match-patch-synchronizer-test.js
+++ tests/synchronizers/diff-match-patch-synchronizer-test.js
@@ -1,93 +1,110 @@
+var assert = require('assert'),
+    jsonpatch = require('fast-json-patch'),
+    DiffMatchPatchSynchronizer = require('../../lib/synchronizers/diff-match-patch');
 
-// public class DiffMatchPatchServerSynchronizerTest {
 
-//     @Test
-//     public void clientDiff() throws Exception {
-//         final ServerSynchronizer<String, DiffMatchPatchEdit> synchronizer = new DiffMatchPatchServerSynchronizer();
-//         final Document<String> document = new DefaultDocument<String>("1234", "test");
-//         final ShadowDocument<String> shadowDocument = shadowDocument("1234", "client1", "testing");
+describe('Diff Match Patch Synchronizer Tests', function () {
+    describe('Create new Diff Match Patch Synchronizer', function () {
+        it('create without new', function () {
+            var diffMatchPatchSynchronizer = DiffMatchPatchSynchronizer();
+            assert.equal(diffMatchPatchSynchronizer instanceof DiffMatchPatchSynchronizer, true, 'should be true');
+        });
+    });
 
-//         final DiffMatchPatchEdit edit = synchronizer.clientDiff(document, shadowDocument);
-//         assertThat(edit.clientVersion(), is(0L));
-//         assertThat(edit.serverVersion(), is(0L));
-//         assertThat(edit.diff().diffs().size(), is(2));
-//         assertThat(edit.diff().diffs().get(0).operation(), is(Operation.UNCHANGED));
-//         assertThat(edit.diff().diffs().get(0).text(), is("test"));
-//         assertThat(edit.diff().diffs().get(1).operation(), is(Operation.ADD));
-//         assertThat(edit.diff().diffs().get(1).text(), is("ing"));
-//     }
+    describe('Diff Match Patch Synchronizer methods', function () {
+        var source, shadow, diffMatchPatchSynchronizer, edit, validDiff, patchMessage, patchDocument, patchedShadow, patchedDocument,
+            documentId = '1234',
+            clientId = 'client1';
 
-//     @Test
-//     public void serverDiff() throws Exception {
-//         final ServerSynchronizer<String, DiffMatchPatchEdit> synchronizer = new DiffMatchPatchServerSynchronizer();
-//         final Document<String> document = new DefaultDocument<String>("1234", "test");
-//         final ShadowDocument<String> shadowDocument = shadowDocument("1234", "client1", "testing");
+        source = {
+            id: documentId,
+            clientId: clientId
+        };
 
-//         final DiffMatchPatchEdit edit = synchronizer.serverDiff(document, shadowDocument);
-//         assertThat(edit.clientVersion(), is(0L));
-//         assertThat(edit.serverVersion(), is(0L));
-//         assertThat(edit.diff().diffs().size(), is(2));
-//         assertThat(edit.diff().diffs().get(0).operation(), is(Operation.UNCHANGED));
-//         assertThat(edit.diff().diffs().get(0).text(), is("test"));
-//         assertThat(edit.diff().diffs().get(1).operation(), is(Operation.DELETE));
-//         assertThat(edit.diff().diffs().get(1).text(), is("ing"));
-//     }
+        shadow = {
+            document: {
+                id: documentId,
+                clientId: clientId
+            }
+        };
 
-//     @Test
-//     public void patchShadow() throws Exception {
-//         final ServerSynchronizer<String, DiffMatchPatchEdit> synchronizer = new DiffMatchPatchServerSynchronizer();
-//         final Document<String> document = new DefaultDocument<String>("1234", "test");
-//         final ShadowDocument<String> shadowDocument = shadowDocument("1234", "client1", "testing");
+        beforeEach(function () {
+            diffMatchPatchSynchronizer = DiffMatchPatchSynchronizer();
+            source.content = null;
+            shadow.document.content = null;
+        });
 
-//         final DiffMatchPatchEdit edit = synchronizer.serverDiff(document, shadowDocument);
-//         final ShadowDocument<String> patchedShadow = synchronizer.patchShadow(edit, shadowDocument);
-//         assertThat(patchedShadow.document().content(), equalTo("test"));
-//     }
+        it('test the client diff', function () {
+            source.content = 'test';
 
-//     @Test
-//     public void patchShadowFromClientDiff() throws Exception {
-//         final ServerSynchronizer<String, DiffMatchPatchEdit> synchronizer = new DiffMatchPatchServerSynchronizer();
-//         final Document<String> document = new DefaultDocument<String>("1234", "Beve");
-//         final ShadowDocument<String> shadowDocument = shadowDocument("1234", "client1", "I'm the man");
+            shadow.clientVersion = 0;
+            shadow.serverVersion = 0;
+            shadow.document.content = 'testing';
 
-//         final DiffMatchPatchEdit edit = synchronizer.clientDiff(document, shadowDocument);
-//         final ShadowDocument<String> patchedShadow = synchronizer.patchShadow(edit, shadowDocument);
-//         assertThat(patchedShadow.document().content(), equalTo("I'm the man"));
-//     }
+            edit = diffMatchPatchSynchronizer.clientDiff(source, shadow);
+            assert.equal(edit.clientVersion, 0);
+            assert.equal(edit.serverVersion, 0);
+            assert.equal(edit.diff.length, 2, 'should be 2 diffs');
+            assert.equal(edit.diff[0].operation, 'UNCHANGED', 'first diff operation should be UNCHANGED');
+            assert.equal(edit.diff[0].text, 'test', 'test text should remain unchanged');
+            assert.equal(edit.diff[1].operation, 'ADD', 'second diff operation should be ADD');
+            assert.equal(edit.diff[1].text, 'ing', 'added text is ing');
+        });
 
-//     @Test
-//     public void patchShadowFromClientDiffCustomEdit() throws Exception {
-//         final ServerSynchronizer<String, DiffMatchPatchEdit> synchronizer = new DiffMatchPatchServerSynchronizer();
-//         final ShadowDocument<String> shadowDocument = shadowDocument("1234", "client1", "Beve");
+        it('test the server diff', function () {
+            source.content = 'test';
 
-//         final DiffMatchPatchEdit edit1 = DiffMatchPatchEdit.withChecksum("bogus")
-//                 .delete("B")
-//                 .add("I'm th")
-//                 .unchanged("e")
-//                 .delete("ve")
-//                 .add(" man")
-//                 .build();
-//         final ShadowDocument<String> patchedShadow = synchronizer.patchShadow(edit1, shadowDocument);
-//         assertThat(patchedShadow.document().content(), equalTo("I'm the man"));
-//     }
+            shadow.clientVersion = 0;
+            shadow.serverVersion = 0;
+            shadow.document.content = 'testing';
 
-//     @Test
-//     public void patchDocument() throws Exception {
-//         final ServerSynchronizer<String, DiffMatchPatchEdit> synchronizer = new DiffMatchPatchServerSynchronizer();
-//         final Document<String> document = new DefaultDocument<String>("1234", "test");
-//         final ShadowDocument<String> shadowDocument = shadowDocument("1234", "client1", "testing");
+            edit = diffMatchPatchSynchronizer.serverDiff(source, shadow);
+            assert.equal(edit.clientVersion, 0);
+            assert.equal(edit.serverVersion, 0);
+            assert.equal(edit.diff.length, 2, 'should be 2 diffs');
+            assert.equal(edit.diff[0].operation, 'UNCHANGED', 'first diff operation should be UNCHANGED');
+            assert.equal(edit.diff[0].text, 'test', 'test text should remain unchanged');
+            assert.equal(edit.diff[1].operation, 'DELETE', 'second diff operation should be DELETE');
+            assert.equal(edit.diff[1].text, 'ing', 'deleted text is ing');
+        });
 
-//         final DiffMatchPatchEdit edit = synchronizer.clientDiff(document, shadowDocument);
-//         final Document<String> patchedDocument = synchronizer.patchDocument(edit, document);
-//         assertThat(patchedDocument.content(), equalTo("testing"));
-//     }
+        it('test patch shadow', function () {
+            source.content = 'Beve';
 
-//     private static ShadowDocument<String> shadowDocument(final String documentId,
-//                                                          final String clientVersion,
-//                                                          final String content) {
-//         return new DefaultShadowDocument<String>(0L,
-//                 0L,
-//                 new DefaultClientDocument<String>(documentId, clientVersion, content));
+            shadow.clientVersion = 0;
+            shadow.serverVersion = 0;
+            shadow.document.content = 'Cool Guy';
 
-//     }
-// }
+
+            edit = diffMatchPatchSynchronizer.serverDiff(source, shadow);
+            patchedShadow = diffMatchPatchSynchronizer.patchShadow(edit, shadow);
+            assert.equal(patchedShadow.document.content, 'Beve', 'shadow test should be test');
+        });
+
+        it('test patch shadow from client diff', function () {
+            source.content = 'Beve';
+
+            shadow.clientVersion = 0;
+            shadow.serverVersion = 0;
+            shadow.document.content = 'Cool Dude';
+
+
+            edit = diffMatchPatchSynchronizer.clientDiff(source, shadow);
+            patchedShadow = diffMatchPatchSynchronizer.patchShadow(edit, shadow, source);
+            assert.equal(patchedShadow.document.content, 'Cool Dude', 'shadow test should be Cool Dude');
+        });
+
+        it('test patch document', function () {
+            source.content = 'test';
+
+            shadow.clientVersion = 0;
+            shadow.serverVersion = 0;
+            shadow.document.content = 'testing';
+
+
+            edit = diffMatchPatchSynchronizer.clientDiff(source, shadow);
+            patchedDocument = diffMatchPatchSynchronizer.patchDocument(edit, source);
+            assert.equal(patchedDocument.content, 'testing', 'document test should be testing');
+        });
+    });
+});
